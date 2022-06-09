04f3264b2665ce0fa160ca0895556ce922e56839
Harvey Francois <har.francois@gmail.com>
2022-06-09 02:25:41 -0700

    Purple

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16fea8d7de8e39e1"; }
+/******/ 		__webpack_require__.h = function() { return "427789af87cddee7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer({ count }) {
+  return (
+      <footer className='p-5 text-2xl bg-purple-800'>
+          <a>{count} Locations World Wide</a>
+      </footer>
+  )
+}
